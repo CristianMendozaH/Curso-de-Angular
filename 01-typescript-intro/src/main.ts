0fec50e8bfe048242b0e3b3f7b1cc098aa2ace20
CristianMendozaH <cristian8mendoza8@gmail.com>
2025-08-19 15:50:42 -0600

    Progreso en el trabajo-S3-C16-TareaP

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -4,7 +4,9 @@ import './style.css'
 
 //import './topics/02-object-interface';
 
-import './topics/03-functions';
+//import './topics/03-functions';
+
+import './topics/04-homework-types';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 

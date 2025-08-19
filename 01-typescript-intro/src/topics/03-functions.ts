963cdf7749fb11cc4f42ae6a24123be1befa69e7
CristianMendozaH <cristian8mendoza8@gmail.com>
2025-08-19 15:42:31 -0600

    Progreso en el trabajo-S3-C15

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -3,6 +3,8 @@
 //Si no se sabe el tipo de retorno, se puede usar `void`
 //si no sabe el tipo de datos se puede usar `any`
 
+
+
 function addNumbers(a: number, b: number) :number{
     return a + b;
 }
@@ -16,11 +18,40 @@ function multiply ( fistNumber: number, secondNumber?: number, base:number = 2) 
 }   
 
 
-const resul: number = addNumbers(1, 2);   
-const resul2: string = addNumbersArrow(1, 2);
-const multiplyResult: number= multiply(5);
+// const resul: number = addNumbers(1, 2);   
+// const resul2: string = addNumbersArrow(1, 2);
+// const multiplyResult: number= multiply(5);
+// console.log({resul, resul2, multiplyResult});
 
-console.log({resul, resul2, multiplyResult});
+
+// Funciones objetos como argumentos
+
+interface Character {
+    name: string;
+    ph: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.ph += amount;
+}
+
+
+const Aragon: Character = {
+    name: 'Aragon',
+    ph: 50,
+    showHp() {
+        console.log(`Puntos de vida: ${this.ph}`);
+    }
+}
+
+healCharacter (Aragon, 20);
+
+healCharacter (Aragon, 10);
+
+Aragon.showHp();
 
 
 export {};
+
+
